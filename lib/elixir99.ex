085f7ee98211ab55e4193db51d1c2f6d6c630267
Nikita Tyrkin <tyrkinn@pm.me>
2025-07-19 18:40:00 +0300

    feat: Implement `last_two` and tests for it

--- lib/elixir99.ex
+++ lib/elixir99.ex
@@ -6,4 +6,13 @@ defmodule Elixir99 do
       [_ | tail] -> last(tail)
     end
   end
+
+  def last_two(xs) do
+    case xs do
+      [] -> {:error, "List is empty"}
+      [_] -> {:error, "List should contain at least 2 elements"}
+      [x, y] -> {:ok, {x, y}}
+      [_ | tail] -> last_two(tail)
+    end
+  end
 end
--- test/elixir99_test.exs
+++ test/elixir99_test.exs
@@ -2,11 +2,23 @@ defmodule Elixir99Test do
   use ExUnit.Case
   doctest Elixir99
 
-  test "last returning error when list is empty" do
+  test "`last` returns error when list is empty" do
     assert {:error, _} = Elixir99.last([])
   end
 
-  test "last returning last element" do
+  test "`last` returns last element" do
     assert {:ok, 5} = Elixir99.last([2, 3, 45, 5])
+  end
+
+  test "`last_two` returns error when list is empty" do
+    assert {:error, _} = Elixir99.last_two([])
+  end
+
+  test "`last_two` returns error when list contains only one value" do
+    assert {:error, _} = Elixir99.last_two([13])
+  end
+
+  test "`last_two` returns last two elements" do
+    assert {:ok, {45, 5}} = Elixir99.last_two([2, 3, 45, 5])
   end
 end
